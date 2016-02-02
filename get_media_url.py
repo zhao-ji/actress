d67e6c835156e7021b4419848f10fbe57c49bb72
p0f <i@minganci.org>
2016-02-02 16:23:33 +0800

    fix: just for debug

--- get_media_url.py
+++ get_media_url.py
@@ -7,9 +7,8 @@ from twitter import Api
 
 from auth import access_token_key, access_token_secret
 from auth import consumer_key, consumer_secret
-from auth import redis_db, redis_psw
 
-redis = StrictRedis(db=redis_db, password=redis_psw)
+redis = StrictRedis(db=13)
 
 api = Api(
     consumer_key, consumer_secret,
@@ -21,8 +20,8 @@ if __name__ == "__main__":
         from pprint import pprint
         if u'entities' in status and u'media' in status['entities']:
             pprint(status)
-            media_list = status['entities']['media']
-            media_url = map(lambda media: media['media_url'], media_list)
-            for item in media_url:
-                redis.rpush("media_url", item)
-                logbook.info(item)
+            # media_list = status['entities']['media']
+            # media_url = map(lambda media: media['media_url'], media_list)
+            # for item in media_url:
+            #     redis.rpush("media_url", item)
+            #     logbook.info(item)
