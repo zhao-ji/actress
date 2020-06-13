cd2be85a44769d3b464192552368fb9b80887abe
Subs <BytesWarning@gmail.com>
2020-06-14 01:31:54 +1200

    Issue #master - photo zoom in is finished

--- static/src/components.js
+++ static/src/components.js
@@ -3,9 +3,12 @@ import {
     useState,
 } from 'react';
 
+import Image from "react-image-enlarger";
+
 
 export function Photo({ image }) {
     const [dimension, updateDimension] = useState(5);
+    const [zoomed, setZoomed] = React.useState(false);
     function onLoad({ target:img }) {
         if (!img.complete) return;
         const width = img.naturalWidth;
@@ -16,13 +19,17 @@ export function Photo({ image }) {
         <div className="photo" style={{
             gridRow: `span ${dimension}`
         }}>
-            <img 
-                src={`https://actress-image.s3-ap-southeast-2.amazonaws.com/${image.Key}`}
-                alt="pic"
+            <Image
                 style={{
                     width: "10rem",
                 }}
+                src={`https://actress-image.s3-ap-southeast-2.amazonaws.com/${image.Key}`}
+                enlargedSrc={`https://actress-image.s3-ap-southeast-2.amazonaws.com/${image.Key}`}
+                alt="pic"
                 onLoad={onLoad}
+                zoomed={zoomed}
+                onClick={() => setZoomed(true)}
+                onRequestClose={() => setZoomed(false)}
             />
         </div>
     );
