a026d6566a19ab7b1b5914734f0516399035f9bb
Subs <BytesWarning@gmail.com>
2020-06-14 02:15:53 +1200

    Issue #master - update the loading status

--- static/src/app.js
+++ static/src/app.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 import { Photo } from './components';
-
-const apiUrl = "https://api.article.minganci.org/actress/?limit=100";
+import { touchBottom, apiUrl } from './utils';
 
 function App() {
     const [isLoading, updateIsLoading] = useState(false);
     const [images, updateImages] = useState([]);
     useEffect(() => {
+        updateIsLoading(true);
         fetch(apiUrl)
             .then(response => response.json())
             .then(data => updateImages([...data]))
@@ -18,13 +18,9 @@ function App() {
     useEffect(() => {
         let anchor = images && images.length > 0 && images[images.length - 1].Key;
         const scrollCheck = () => {
-            const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
-            const body = document.body;
-            const html = document.documentElement;
-            const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-            const windowBottom = windowHeight + window.pageYOffset;
-            if (windowBottom >= docHeight) {
+            if (touchBottom()) {
                 const param = anchor ? `&fromKey=${anchor}` : "";
+                updateIsLoading(true);
                 fetch(apiUrl + param)
                     .then(response => response.json())
                     .then(data => {
@@ -46,9 +42,9 @@ function App() {
     return (
       <div id="App">
         <header className="App-header">
-          <p>
-              Life recording and photo gallery
-          </p>
+            <p>
+                Life recording and photo gallery
+            </p>
         </header>
         <div id="gallary">
             {
---
+++ static/src/utils.js
@@ -0,0 +1,10 @@
+export const apiUrl = "https://api.article.minganci.org/actress/?limit=100";
+
+export function touchBottom() {
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    return windowBottom >= docHeight;
+}
